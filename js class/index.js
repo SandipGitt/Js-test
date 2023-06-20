6498ce1208862a9d6abb463d7c579dc70b65afb2
Sandip Neupane <sandipneupane775@gmail.com>
2023-06-20 14:30:52 +0545

    learned about destructing objects in array

--- js class/index.js
+++ js class/index.js
@@ -119,3 +119,9 @@ const{password,phone, ...rest}= User;
 console.log({rest});
 
 
+const {a, ...others} = {a:1,b:2,c:3};
+console.log(others);
+
+const[first, ...others2] = [1,2,3];
+console.log(others2);
+
